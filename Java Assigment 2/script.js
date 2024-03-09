7e331595eb59c325bfeafb0fc728ccacb74658cb
sameer hussain <154673624+sameerhussain11@users.noreply.github.com>
2024-03-09 22:12:28 +0500

    Get input from user and add 3 numbers and show result on console
    Get input from user and add 3 numbers and show result on console

---
+++ Java Assigment 2/script.js
@@ -0,0 +1,11 @@
+alert("Asalamoalikom Sir ! 😀");
+alert("For Adding Numbers Input Value");
+let input1 = parseFloat(prompt("Value 1"));
+let input2 = parseFloat(prompt("Value 2"));
+let input3 = parseFloat(prompt("Value 3"));
+let sum = input1 + input2 + input3;
+
+console.log("Answer : " +sum);
+alert("Answer is : " +sum);
+
+//coded by Sameer hussain //
