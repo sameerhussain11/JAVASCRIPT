883366f29f49365dbca4ce44f4672012704e85b8
sameer hussain <154673624+sameerhussain11@users.noreply.github.com>
2024-03-09 22:12:00 +0500

    Add files via upload

--- Java Assigment 1/script.js
+++ Java Assigment 1/script.js
@@ -1,11 +1,11 @@
-alert("Asalamoalikom Sir ! 😀")
-alert("Input Data For Output ! sir 😀")
+alert("Asalamoalikom Sir ! 😀");
+alert("Input Data For Output ! sir 😀");
 
-let first = prompt("First Name")
-let middle = prompt("Middle Name")
-let last = prompt("Last Name")
+let first = prompt("First Name");
+let middle = prompt("Middle Name");
+let last = prompt("Last Name");
 
-console.log("Well come to Assigment Number 1 " + first + " " + middle + " " + last)
-alert("Well come " + first + " " + middle + " " +last)
+console.log("Well come to Assigment Number 1 " + first + " " + middle + " " + last);
+alert("Well come " + first + " " + middle + " " +last);
 
 // Coded By Sameer Hussain //
