221eced93c04efa8df17a7a94ee590db2515ccff
sameer hussain <154673624+sameerhussain11@users.noreply.github.com>
2024-03-13 23:57:49 +0500

    Get data from user and print the result grade of students by using else if statement
    Get data from user and print the result grade of students by using else if statement

---
+++ Java Assigment 3/script.js
@@ -0,0 +1,28 @@
+alert("Asalamoalikom sir ! 😀");
+alert("Find Your Grade Using Numbers ! 😀");
+let input1 = parseFloat(prompt("Search Your Result here"));
+
+if (input1 >=90 ) {
+    console.log("your grade is : A-one ")
+    alert("Excellent 🔥🙌");
+}
+else if (input1 >=80 ) {
+    console.log("your grade is : A+ ")
+    alert("Very Good 👍");
+}
+else if (input1 >=70 ) {
+    console.log("your grade is : B ")
+    alert("Good 👍");
+}
+else if (input1 >=60 ) {
+    console.log("your grade is : C ")
+    alert("Average 👍");
+}
+else if (input1 >=50 ) {
+    console.log("your grade is : D ")
+    alert("Need Improvement 😀");
+}
+else if (input1 <=50 ) {
+    console.log("Your grade is : F")
+    alert("Need Hardwork 🙂");
+}
